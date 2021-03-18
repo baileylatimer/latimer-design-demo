e0956de41f65890801a4fd94f8a8f83f1ea242ba
Bailey Latimer <latimerdesignco@gmail.com>
2021-03-18 16:52:05 -0400

    added padding-top to /cart + /product page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -4,8 +4,8 @@ import Cart from '~/components/Cart'
 import { Container } from '~/utils/styles'
 
 const CartPage = () => (
-  <Container>
-    <h1>Cart</h1>
+  <Container className="padding-top-xl">
+    <h1>Your cart</h1>
     <Cart />
   </Container>
 )
